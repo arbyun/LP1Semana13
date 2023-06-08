0d1a164cae483034fe7d46836b7c252b2240dbf0
arbyun <someonealiveh@gmail.com>
2023-06-08 12:29:42 +0100

    flesh out IsPalindrome function to determine if string is palindrome

--- Palindrome/Program.cs
+++ Palindrome/Program.cs
@@ -8,10 +8,17 @@ namespace Palindrome
         {
             Console.WriteLine("Hello, World!");
         }
-
+        
         static bool IsPalindrome(string str)
         {
+            if (str == null)
+            {
+                throw new ArgumentNullException(nameof(str));
+            }
+
             
+
+            return IsRecursive(str);
         }
 
         static bool IsRecursive(string str)
