fb7168e8d02dad7961660e338f789d9f022aa2d3
arbyun <someonealiveh@gmail.com>
2023-06-08 12:37:20 +0100

    create GetMinMax1 method

--- MultipleStuff/SuperList.cs
+++ MultipleStuff/SuperList.cs
@@ -4,6 +4,20 @@ namespace MultipleStuff
 {
     internal class SuperList : List<double>
     {
-        
+        public (double min, double max) GetMinMax1()
+        {
+            double min = double.MaxValue;
+            double max = double.MinValue;
+
+            foreach (double num in this)
+            {
+                if (num < min)
+                    min = num;
+                if (num > max)
+                    max = num;
+            }
+
+            return (min, max);
+        }
     }
 }
